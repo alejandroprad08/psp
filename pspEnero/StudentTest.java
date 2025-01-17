e5dccb2dbe9c50e44fc03f05c2f151683a66b46d
alejandroprad08 <166373411+alejandroprad08@users.noreply.github.com>
2025-01-17 12:21:02 +0100

    Add files via upload

---
+++ pspEnero/StudentTest.java
@@ -0,0 +1,16 @@
+package pspEnero;
+
+public class StudentTest {
+	public static void main(String[] args) throws InterruptedException{
+		Student student1 = new Student("Elghetto", 10);
+		Student student2 = new Student("Sergi", 15);
+		Student student3 = new Student("Marquis", 17);
+		
+		student1.start();
+		student2.start();
+		student3.start();
+		
+		Thread.sleep(200);
+		System.out.println("All students are doing the exam");
+	}
+}
